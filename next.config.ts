1a0d5dff2042c7764444df2826bccfc11dbb014e
naotokato111 <naotokato620@gmail.com>
2024-10-29 15:03:55 +0900

    refactor: use typescript for next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   // logging: {
   //   fetches: {
   //     fullUrl: true,
@@ -22,4 +23,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
